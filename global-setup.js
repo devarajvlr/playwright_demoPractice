29fa2401664d2dceae25a664e7bbac9421e47ccf
devarajvlr <devarajkm.18@gmail.com>
2026-02-15 22:17:54 +0530

    headless true

--- global-setup.js
+++ global-setup.js
@@ -1,7 +1,7 @@
 const { chromium } = require('@playwright/test');
 
 module.exports = async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://practicetestautomation.com/practice-test-login/');
